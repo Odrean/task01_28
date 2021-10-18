be3fe47ea6ff50f6b062cd995b63472d69f05dc8
Odrean <90618288+Odrean@users.noreply.github.com>
2021-10-19 02:07:49 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/kochergin/Main.java
@@ -0,0 +1,135 @@
+package ru.vsu.cs.kochergin;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+        
+        double firstCakePrice = readCakePrice("firstCake");
+        double secondCakePrice = readCakePrice("secondCake");
+
+        int firstCakeQuantity = readQuantityCake("firstCake");
+        int secondCakeQuantity = readQuantityCake("secondCake");
+
+        double budgetUser = readBudgetUser(firstCakePrice, secondCakePrice);
+
+        int maxCakesUserCanBuy = calculateMaxCakesUserCanBuy(budgetUser, firstCakePrice, secondCakePrice, firstCakeQuantity, secondCakeQuantity);
+
+        System.out.printf("Maximum quantity: %d", maxCakesUserCanBuy);
+    }
+
+    public static double readBudgetUser(double firsCakePrice, double secondCakePrice) {
+        Scanner scanner = new Scanner(System.in);
+        double budgetUser = 0;
+
+        System.out.printf("print your budget: ");
+
+        if (!scanner.hasNextDouble()) {
+            System.out.println("incorrect data");
+            readBudgetUser(firsCakePrice, secondCakePrice);
+        }
+
+        budgetUser = scanner.nextDouble();
+
+        if (checkBudgetUser(budgetUser, firsCakePrice, secondCakePrice)) {
+            System.out.printf("The user's budget: %.2f%n", budgetUser);
+
+        } else {
+            System.out.print("The budget is too small, print more");
+            readBudgetUser(firsCakePrice, secondCakePrice);
+        }
+
+        return budgetUser;
+    }
+
+    public static boolean checkBudgetUser(double budgetUser, double firstCakePrice, double secondCakePrice) {
+        if (budgetUser >= firstCakePrice || budgetUser >= secondCakePrice) {
+            return true;
+        }
+        return false;
+    }
+
+    public static double readCakePrice(String cakeName) {
+        Scanner scanner = new Scanner(System.in);
+        double firstCakePrice = 0;
+
+        System.out.printf("Print the price of the %s: ", cakeName);
+
+        if (scanner.hasNextDouble()) {
+            firstCakePrice = scanner.nextDouble();
+            System.out.printf("The cake's price: %.2f%n", firstCakePrice);
+        } else {
+            System.out.println("incorrect data");
+            readCakePrice(cakeName);
+        }
+
+        return firstCakePrice;
+    }
+
+    public static int readQuantityCake(String cakeName) {
+        System.out.printf("print the quantity of the %s: ", cakeName);
+        Scanner scanner = new Scanner(System.in);
+        int quantityCake = 0;
+
+        if (scanner.hasNextInt()) {
+            quantityCake = scanner.nextInt();
+            System.out.printf("quantity of cakes: %d%n", quantityCake);
+        } else {
+            System.out.println("incorrect data");
+            readQuantityCake(cakeName);
+        }
+
+        return quantityCake;
+    }
+
+    public static int calculateMaxCakesUserCanBuy(double budgetUser, double firstCakePrice,
+                                                  double secondCakePrice, int firstCakeQuantity,
+                                                  int secondCakeQuantity) {
+        int maxCakesUserCanBuy = 0;
+        double minCakePrice;
+        double maxCakePrice;
+        double firstSellingCakeQuantity;
+        double secondSellingCakeQuantity;
+
+        if (firstCakePrice <= secondCakePrice) {
+            minCakePrice = firstCakePrice;
+            maxCakePrice = secondCakePrice;
+
+            firstSellingCakeQuantity = firstCakeQuantity;
+            secondSellingCakeQuantity = secondCakeQuantity;
+        } else {
+            minCakePrice = secondCakePrice;
+            maxCakePrice = firstCakePrice;
+
+            firstSellingCakeQuantity = secondCakeQuantity;
+            secondSellingCakeQuantity = firstCakeQuantity;
+        }
+
+        while (firstSellingCakeQuantity > 0) {
+            budgetUser -= minCakePrice;
+
+            if (budgetUser < 0) {
+                break;
+            }
+
+            maxCakesUserCanBuy++;
+            firstSellingCakeQuantity--;
+        }
+
+        while (secondSellingCakeQuantity > 0) {
+            budgetUser -= maxCakePrice;
+
+            if (budgetUser < 0) {
+                break;
+            }
+
+            maxCakesUserCanBuy++;
+            secondSellingCakeQuantity--;
+        }
+
+        return maxCakesUserCanBuy;
+    }
+}
